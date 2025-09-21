eb4cdf9d9ba2a1f8c18b151c6aba55e98c703e99
Alex Suslov <suslov.lexx@gmail.com>
2025-09-21 21:37:37 +0500

    upd config.py

--- src/app/config.py
+++ src/app/config.py
@@ -1,52 +1,54 @@
-import os
+import sys
+from pathlib import Path
 
 from dotenv import dotenv_values
 
 
-env_values = dotenv_values(".env")
-if not env_values:
-    raise ValueError("Must specify envs in .env file")
+BASE_DIR = Path(__file__).parent.absolute()
+ENV_FILE_PATH = BASE_DIR / ".env"
 
 
 class Settings:
     """Класс с настройками приложения"""
 
     def __init__(self, values: dict):
-        self.values: dict = values
-        self.host_dir: str = self._get_host_dir()
-        self.cloud_dir: str = self._get_cloud_dir()
-        self.auth_token: str = self._get_auth_token()
-        self.sync_period: int = self._get_sync_period()
-        self.log_file_path: str = self._get_log_file_path()
+        """Инициализация объекта"""
+        self.__values: dict = values
+        self.host_dir: Path = self.__get_host_dir()
+        self.cloud_dir: str = self.__get_cloud_dir()
+        self.auth_token: str = self.__get_auth_token()
+        self.sync_period: int = self.__get_sync_period()
+        self.log_file_path: Path = self.__get_log_file_path()
 
     def _get_env_value(self, env_name) -> str:
         """Получаем значение переменной окружения, если оно есть"""
-        env_value = self.values.get(env_name)
+        env_value = self.__values.get(env_name)
         if not env_value:
             raise EnvironmentError(
                 f"Значение переменной окружения '{env_name}' не найдено"
             )
         return env_value
 
-    def _get_host_dir(self) -> str:
+    def __get_host_dir(self) -> Path:
         """Получаем путь к синхронизируемой папке на хосте"""
         host_dir_path_env_name = "HOST_DIR_PATH"
-        host_dir = self._get_env_value(host_dir_path_env_name)
+        host_dir = Path(self._get_env_value(host_dir_path_env_name))
 
-        is_exists = os.path.exists(host_dir)
-        if not is_exists:
+        if not host_dir.exists():
             raise FileNotFoundError(f"Путь '{host_dir}' не существует")
+        if not host_dir.is_dir():
+            raise NotADirectoryError(f"Путь '{host_dir}' должен быть директорией")
 
         return host_dir
 
-    def _get_cloud_dir(self) -> str:
+    def __get_cloud_dir(self) -> str:
         """Получаем имя папки в облачном хранилище"""
         cloud_dir_env_name = "CLOUD_DIR"
         cloud_dir = self._get_env_value(cloud_dir_env_name)
 
         return cloud_dir
 
-    def _get_auth_token(self) -> str:
+    def __get_auth_token(self) -> str:
         """Получаем токен аутентификации"""
         auth_token_env_name = "AUTH_TOKEN"
         auth_token = self._get_env_value(auth_token_env_name)
@@ -54,7 +56,7 @@ class Settings:
             raise ValueError("Неверный токен")
         return auth_token
 
-    def _get_sync_period(self) -> int:
+    def __get_sync_period(self) -> int:
         """Получаем период синхронизации в секундах"""
         sync_period_env_name = "SYNC_PERIOD"
         sync_period = self._get_env_value(sync_period_env_name)
@@ -62,21 +64,39 @@ class Settings:
         try:
             sync_period = int(sync_period)
         except ValueError as exc:
-            print(f"Ошибка: значение периода синхронизации нужно указать цифрами")
+            print(f"Значение периода синхронизации нужно указать цифрами")
             raise exc
 
         return sync_period
 
-    def _get_log_file_path(self) -> str:
+    def __get_log_file_path(self) -> Path:
         """Получаем путь к файлу лога"""
         log_file_path_env_name = "LOG_FILE_PATH"
-        log_file_path = self._get_env_value(log_file_path_env_name)
+        log_file_path = Path(self._get_env_value(log_file_path_env_name))
 
-        is_exists: bool = os.path.exists(log_file_path)
-        if not is_exists:
+        if not log_file_path.exists():
             raise FileNotFoundError(f"Путь '{log_file_path}' не существует")
 
         return log_file_path
 
 
-settings = Settings(values=env_values)
+def get_settings() -> Settings:
+    """Получаем экземпляр класса Setting"""
+    try:
+        if not ENV_FILE_PATH.exists():
+            raise FileNotFoundError("Не найден .env файл")
+
+        env_values = dotenv_values(dotenv_path=ENV_FILE_PATH)
+        if not env_values:
+            raise ValueError(
+                "Необходимо указать переменные окружения и их значения в .env файле"
+            )
+
+        return Settings(values=env_values)
+
+    except (EnvironmentError, FileNotFoundError, NotADirectoryError, ValueError) as exc:
+        print(f"Ошибка: {exc}")
+        sys.exit(1)
+
+
+settings = get_settings()
